4ef3c40b5d0448d0b512a64f0d5cc45d0f877ffd
dike-code <dikeclintonobinna58@gmail.com>
2025-08-13 22:47:22 +0100

    UPDATE

--- script/main.js
+++ script/main.js
@@ -14,4 +14,22 @@ const swiper = new Swiper(".swiper", {
 		nextEl: ".next",
 		prevEl: ".prev",
 	},
+
+	breakpoints: {
+		// When window width is >= 320px
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		// When window width is >= 640px
+		640: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+		},
+		// When window width is >= 1024px
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+	},
 });
